28f7359ad6a88bad2cdc29369f5be2382f36277e
Ian Ducloux <ianducloux@gmail.com>
2020-02-26 13:23:05 -0600

    Metodos de manipulacion de archivos txt

--- main.py
+++ main.py
@@ -7,4 +7,29 @@ Created on Thu Feb 20 12:25:15 2020
 
 Procesador de notas de texto.
 """
+#CONFIGURACION DE LIBRERIAS
+import spacy
+nlp = spacy.load("es_core_news_sm")
+#FIN LIBCONFIG
 
+# =============================================================================
+# #CREAR ARCHIVO DE TEXTO
+# f = open('texto.txt','wb')
+# f.write('Si puedes leer esto el archivo de texto se genero')
+# f.close
+# =============================================================================
+
+# =============================================================================
+# #ESCRIBIR EN ARCHIVO CREADO
+# f=open('texto.txt','a')
+# f.write('\nMensaje')
+# f.close
+# =============================================================================
+
+# =============================================================================
+# #LEER ARCHIVO DE TEXTO
+# f=open('texto.txt','r')
+# mensaje = f.read()
+# print(mensaje)
+# f.close
+# =============================================================================
